89a1e6923e51edfec5a3b7bf21a8b8e6c983b4ea
tigger <tigger@channel.io>
2022-07-18 22:24:39 +0900

    main page에 background color 추가

--- src/components/MainPage/MainPage.styled.ts
+++ src/components/MainPage/MainPage.styled.ts
@@ -5,6 +5,13 @@ export const Main = styled.main`
   display: flex;
   justify-content: center;
   align-items: center;
+  background: linear-gradient(
+    170deg,
+    ${({ foundation }) => foundation?.theme['bgtxt-navy-lighter']},
+    ${({ foundation }) => foundation?.theme['bgtxt-navy-light']} 20%,
+    ${({ foundation }) => foundation?.theme['bgtxt-navy-normal']} 80%,
+    ${({ foundation }) => foundation?.theme['bgtxt-navy-dark']} 95%
+  );
 `
 
 export const Wrapper = styled.div`
@@ -13,9 +20,9 @@ export const Wrapper = styled.div`
   margin: 0 auto;
   padding: 40px;
   ${({ foundation }) => foundation?.rounding.round16}
-  ${({ foundation }) => foundation?.elevation.ev3()}
+  ${({ foundation }) => foundation?.elevation.ev4()}}
 `
 
 export const H1 = styled.h1`
-  font-size: 40px;
+  font-size: 38px;
 `
