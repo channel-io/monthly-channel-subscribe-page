d0109b8607119bc7299ab7db5f18469220ce2195
tigger <tigger@channel.io>
2022-07-25 10:24:23 +0900

    main 엘리먼트에 padding 추가

--- src/components/MainPage/MainPage.styled.ts
+++ src/components/MainPage/MainPage.styled.ts
@@ -2,6 +2,7 @@ import { styled } from '@channel.io/bezier-react'
 
 export const Main = styled.main`
   height: 100vh;
+  padding: 0 20px;
   display: flex;
   justify-content: center;
   align-items: center;
